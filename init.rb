72df0316c9cb181c9cd0ef189d248660e14676ad
Jean-Michel Garnier <jeanmichel@21croissants.com>
2009-01-05 17:28:40 +0100

    Removing all the plugin files

--- init.rb
+++
@@ -1,4 +0,0 @@
-# Include hook code here
-if RAILS_ENV == 'test'
-  require 'rails_unit_testing_helpers'
-end
--- spec/rails_unit_testing_helpers_spec.rb
+++ spec/rails_unit_testing_helpers_spec.rb
@@ -1,20 +1,9 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe "Rails unit testing helpers:" do
-
-  describe "\nEating its own dog food ;-) This plugin" do
-    it { ruby_code_in_rails_plugin("dont_repeat_yourself").
-        with_threshold_of_duplicate_lines(2).
-        with_netbeans_reporting.
-        should be_dry }
-  end
+ 
   
-  describe "rails_application helper" do
-    # We habe some duplicate lines on purpose in the app/models
-    it { rails_application.
-        with_netbeans_reporting.
-        should_not be_dry }
-  end 
+
       
 end
   
